7ddd62cbddcce9a7a53ea6e0959bdbf9d96cd012
Marcus Vinicius Nascimento Ferreira <103126863+alucard93@users.noreply.github.com>
2023-10-14 14:37:44 -0300

    feat(success): criação da página success e estilização

--- src/pages/CheckoutCompleted/index.tsx
+++ src/pages/CheckoutCompleted/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react'
 import { StyledCheckoutCompleted } from './style'
+
+import PinSucess from '../../componentes/svg/PinSucess'
+import ClockSuccess from '../../componentes/svg/ClockSuccess'
+import DollarSuccess from '../../componentes/svg/DollarSuccess'
+import AnimationSuccess from '../../componentes/svg/AnimationSuccess'
 
 export const CheckoutCompleted = () => {
   return (
     <StyledCheckoutCompleted>
-      <h1 className="text-order-confirm">Uhu! Pedido confirmado</h1>
-      <p className="text-order-description">
-        Agora é só aguardar que logo o café chegará até você
-      </p>
-
       <div>
-        <div></div>
+        <h1 className="text-order-confirm">Uhu! Pedido confirmado</h1>
+        <p className="text-order-description">
+          Agora é só aguardar que logo o café chegará até você
+        </p>
+        <div className="border-container-delivery-address">
+          <div className="container-delivery-address">
+            <PinSucess />
+            <p className="text-delivery-address">
+              Entrega em{' '}
+              <strong className="strong-success">
+                Rua João Daniel Martinelli, 102
+              </strong>
+              <br />
+              Farrapos - Porto Alegre, RS
+            </p>
+          </div>
+          <div className="container-delivery-address">
+            <ClockSuccess />
+            <p className="text-delivery-address">
+              Previsão de entrega <br />
+              <strong className="strong-success">20 min - 30 min</strong>
+            </p>
+          </div>
+          <div className="container-delivery-address">
+            <DollarSuccess />
+            <p className="text-delivery-address">
+              Pagamento na entrega <br />
+              <strong className="strong-success">Cartão de Crédito</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <AnimationSuccess />
       </div>
     </StyledCheckoutCompleted>
   )
--- src/pages/CheckoutCompleted/style.ts
+++ src/pages/CheckoutCompleted/style.ts
@@ -1,6 +1,13 @@
 import { styled } from 'styled-components'
 
 export const StyledCheckoutCompleted = styled.div`
+  max-width: 1120px;
+  display: flex;
+  gap: 102px;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin: 0 auto;
+
   .text-order-confirm {
     color: ${({ theme }) => theme['yellow-dark']};
 
@@ -18,5 +25,42 @@ export const StyledCheckoutCompleted = styled.div`
     font-style: normal;
     font-weight: 400;
     line-height: 130%; /* 26px */
+  }
+
+  .border-container-delivery-address {
+    display: flex;
+    width: 100%;
+    max-width: 526px;
+    padding: 40px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 32px;
+    border-radius: 6px 36px;
+    border: 1px solid #dbac2c;
+    margin-top: 40px;
+  }
+
+  .container-delivery-address {
+    display: flex;
+    gap: 12px;
+  }
+
+  .text-delivery-address {
+    color: var(--base-text, #574f4d);
+
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  }
+
+  .strong-success {
+    color: var(--base-text, #574f4d);
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
   }
 `
